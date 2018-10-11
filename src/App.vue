646e1f510bf18c38f450f7ffd3c250d8f2ce47c3
Nave Kevin <kevnave82@gmail.com>
2018-10-11 02:50:05 +0200

    first commit vuex
    a test for vuex

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Product-List-One></Product-List-One>
+    <Product-List-Two></Product-List-Two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'Product-List-One': ProductListOne,
+    'Product-List-Two': ProductListTwo,
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="Product-List-One">
+        <h2>Product List One</h2>
+        <ul>
+            <li v-for="(product,index) in saleProducts" :key="index">
+                <span class="name">{{ product.name }}</span>
+                <span class="price">{{ product.price }}</span>
+            </li>
+        </ul>   
+        <button v-on:click="reducePrice(4)">Reduce Price</button>
+    </div>
+</template>
+
+<script>    
+import {mapGetters} from 'vuex';
+import {mapActions} from 'vuex';
+
+export default {
+  computed: {
+      products(){
+          return this.$store.state.products;
+      },
+      ...mapGetters([
+          'saleProducts'
+      ])
+  }, 
+  methods: {
+      ...mapActions([
+          'reducePrice'
+      ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
