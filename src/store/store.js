b5bfdd650990576dea4c264a7f74816a48778a9d
Nave Kevin <kevnave82@gmail.com>
2018-10-11 23:18:40 +0200

    test

--- src/store/store.js
+++ src/store/store.js
@@ -10,7 +10,7 @@ export const store = new Vuex.Store({
             {name: 'Banana Skin', price: 20},
             {name: 'Shiny Star', price: 40},
             {name: 'Green Shells', price: 60},
-            {name: 'Red Shells', price: 80}
+            {name: 'Red Shellsss', price: 80}
         ]
     },
     getters: {
